25116c6246576fe93a436ed29de427537ec4fac2
TracyMuso <musongoletracy@gmail.com>
2024-10-08 11:53:58 +0200

    feat(hero-section): add styles to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full">
       <Header />
-      <main>
+      <main className="flex flex-col items-center w-full m-0 p-0">
         <Hero />
       </main>
     </div>
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,23 +2,30 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Powering your home with green energy</h1>
-        <p>
-          General Energy is dedicated to providing reliable and sustainable
-          energy for both residential and commercial clients. Save up on energy
-          costs with flexible payment plans and your No 1 solar energy company
-          <button>Get started!</button>
-        </p>
-      </div>
-      <div>
-        <Image
-          src={'/images/man-holding-solar-panel.jpg'}
-          height={640}
-          width={480}
-          alt="man holding solar panel"
-        />
+    <section className="container px-24 py-11 w-full">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-9 w-3/5">
+          <h1 className="text-5xl font-extrabold text-brown-900 leading-14">
+            Powering your home with green energy
+          </h1>
+          <p className="text-xl text-brown-600 font-Inria w-4/5">
+            General Energy is dedicated to providing reliable and sustainable
+            energy for both residential and commercial clients. Save up on
+            energy costs with flexible payment plans and your No 1 solar energy
+            company.
+          </p>
+          <button className="w-[230px] text-xl rounded-xl bg-yellow-500 text-white font-bold px-8 py-4">
+            View packages!
+          </button>
+        </div>
+        <div>
+          <Image
+            src={'/images/man-holding-solar-panel.jpg'}
+            height={640}
+            width={480}
+            alt="man holding solar panel"
+          />
+        </div>
       </div>
     </section>
   )
