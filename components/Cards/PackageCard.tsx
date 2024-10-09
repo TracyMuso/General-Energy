ae9f4258e743fc7478c5dfdf6c262cb82217eab8
TracyMuso <musongoletracy@gmail.com>
2024-10-09 15:40:11 +0200

    feat(packages): style packages section

--- components/Cards/PackageCard.tsx
+++ components/Cards/PackageCard.tsx
@@ -1,21 +1,27 @@
+import type { PackagesType } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
 
-const PackageCard = () => {
+const PackageCard = ({ link, title, icon, text, point1, point2, point3 }: PackagesType) => {
+
   return (
-    <div className="rounded-xl py-4 px-7 m-0 border border-yellow-300 shadow w-full flex flex-col gap-6">
-      <div className="flex gap-2 items-center mx-auto">
-        <Image className="" src='/icons/home-svgrepo-com.svg' width={40} height={40} alt="icon" />   
-        <h4 className="font-Inter text-3xl font-bold">Residential Packages</h4>
+    <Link href={link}>
+      <div className="rounded-xl py-4 px-7 m-0 border border-yellow-300 shadow flex flex-col gap-6">
+        <div className="flex gap-2 items-center mx-auto">
+          <Image className="" src={icon} width={40} height={40} alt="icon" />
+          <h4 className="font-Inter text-3xl font-bold">{title}</h4>
+        </div>
+        <span className="text-xl">{text}</span>
+        <ul className="list-disc pl-6 space-y-6 pb-6">
+          <li>{point1}</li>
+          <li>{point2}</li>
+          <li className="">
+          {point3}</li>
+        </ul>
+        <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg">Learn more</button>
       </div>
-      <span className="text-xl">Power up your home with packages starting from k5000</span>
-      <ul className="list-disc pl-6 space-y-6">
-        <li>24/7 support</li>
-        <li>Free consultation</li>
-        <li className="">
-        Get access to a cost savings calculator</li>
-      </ul>
-      <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg">Learn more</button>
-    </div>
+    </Link>
+
   )
 }
 
--- components/Packages/index.tsx
+++ components/Packages/index.tsx
@@ -1,13 +1,16 @@
+import { PackagesData } from "@/constants/data/LandingPage";
 import PackageCard from "../Cards/PackageCard";
 
 const Packages = () => {
   return (
-    <section className="h-[100vh] px-16">
-      <div>
-        <div className="w-[540px]">
-          <PackageCard />
+    <section className="h-[100vh] px-24 py-12 ">
+      <h1 className="text-5xl font-extrabold text-center font-Inter pb-6">Packages</h1>
+      <div className="py-5">
+        <div className="flex items-center justify-between gap-10">
+          {PackagesData.map((item, idx) => (
+            <PackageCard {...item} key={idx} />
+          ))}
         </div>
-        <div>right card</div>
       </div>
     </section>
   )
