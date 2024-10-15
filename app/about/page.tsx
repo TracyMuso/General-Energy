beff32923039c588e1c78e569bb1c64a5025e5b2
TracyMuso <musongoletracy@gmail.com>
2024-10-15 10:51:59 +0200

    feat(about-page): update the content of the about page first section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,15 +3,17 @@ import Header from "@/components/Layout/Header"
 import { ProductsAndServices } from "@/constants/data/about"
 
 const AboutPage = () => {
-
   return (
     <div className="w-full">
       <Header />
       <section>
         <div>
           <h1>About us</h1>
-          <p>General Energy is based in Lubumbashi in the Haut-Katanga province of the DRC. It was founded in 2019 with the aim of making high quality and modern road traffic lights accessible. We aim to do better by incorporating solar energy and meeting the technical requirements of photovoltaic equipment, and hiring the most qualified professionals.<br /> <br />
-          We carry out the installation and maintenance of:
+          <div>
+            <p>
+            General Energy is based in Lubumbashi in the Haut-Katanga province of the DRC. It was founded in 2019 with the aim of making high quality and modern road traffic lights accessible. We aim to do better by incorporating solar energy and meeting the technical requirements of photovoltaic equipment, and hiring the most qualified professionals.<br /> <br />
+            We carry out the installation and maintenance of:
+            </p>
           <ul>
             {ProductsAndServices.map((item, idx) => (
               <li key={idx}>
@@ -20,7 +22,7 @@ const AboutPage = () => {
             ))}
           </ul>
            We also design and manufature electric <Link href={'/services/cable-fabrication'}>cables and wires</Link> and <Link href={'/products/extension-cords'}>extension cords</Link> .
-          </p>
+          </div>
         </div>
       </section>
       </div>
