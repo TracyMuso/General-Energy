abac39cc0356bd4bec0ec393c6bac67b23c7c262
TracyMuso <musongoletracy@gmail.com>
2024-10-18 14:20:37 +0200

    feat(contact-page): add faq section to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,18 +1,28 @@
+'use client'
 import Link from 'next/link';
 import '../globals.css'
 import Header from "@/components/Layout/Header";
+import Contact from '@/components/Contact';
+import FAQs from '@/components/faqs';
 
 const ContactPage = () => {
   return (
-    <section className="w-full">
+    <div className="w-full">
       <Header />
-      <div className="w-full contact-top h-[83vh] flex flex-col justify-center items-center gap-4 py-12">
+      <section className="w-full contact-top h-[83vh] flex flex-col justify-center items-center gap-4 py-12">
         <h1 className='font-bold text-4xl pb-4'>We are here for you</h1>
         <p className='text-xl w-4/5 text-center pb-6'>General Energy has been serving customers excellently since 2019. Please the use the available platforms to get in touch. Or come visit us at the office. <br /> Please check out the frequently asked questions below, </p>
         <Link className='bg-white px-6 py-3 text-center rounded-xl font-bold' href={'#contact'}> Go to Contact form</Link>
         <Link className='bg-yellow-500 px-6 py-3 text-center rounded-xl font-bold' href={'#faq'}> Go to FAQ</Link>
-      </div>
-    </section>
+      </section>
+      <section id='contact' className='w-full flex flex-col gap-7 items-center py-16'>
+        <h1 className='text-4xl font-bold'>Get in touch</h1>
+        <Contact />
+      </section>
+      <section id='faq' className='px-20 pt-6 pb-16'>
+        <FAQs />
+      </section>
+    </div>
   )
 }
 
---
+++ components/faqs/index.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Image from "next/image";
+import questions from "./questions";
+
+const FAQs = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const handleOpenAnswer = (index: number) => {
+    setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
+  return (
+    <>
+      <div className="text-center">
+        <div className="w-4/5 mx-auto">
+          <h2 className="text-[48px] md:text-3xl pb-8 max-[640px]:text-2xl lg:text-4xl font-extrabold">
+            Frequently Asked Questions
+          </h2>
+        </div>
+      </div>
+      <ul className="px-2 md:px-0 space-y-5">
+        {questions.map((question, index) => (
+          <li
+            key={question.question}
+            className={`${
+              activeIndex === index ? "bg-[#F8DEB0]" : "bg-transparent"
+            } hover:bg-[#F8DEB0] rounded-lg cursor-pointer p-4`}
+            onClick={() => handleOpenAnswer(index)}
+          >
+            <div className="flex justify-between items-center">
+              <p className="text-base md:text-xl font-medium max-[500px]:text-m">
+                {question.question}
+              </p>
+              <div
+                className={
+                  activeIndex === index
+                    ? "rotate-180 duration-300"
+                    : "duration-300"
+                }
+              >
+                <Image src={'/icons/arrow-down-svgrepo-com.svg'} width={40} height={40} alt="arrow down icon" />
+              </div>
+            </div>
+
+            {activeIndex === index ? (
+              <div className="transition ease-in-out delay-150">
+                <p className="mt-2">{question.answer}</p>
+              </div>
+            ) : null}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default FAQs;
