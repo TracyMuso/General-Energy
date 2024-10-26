9ebfd8d83ae0655c0ebf5a390e50a6738e7ab1ff
TracyMuso <musongoletracy@gmail.com>
2024-10-26 11:27:57 +0200

    feat(responsive-page-design): make services section responsive

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -6,17 +6,19 @@ const Hero = () => {
   return (
     <section id="hero" className="px-0 lg:pb-0 md:pb-20 w-full hero-section lg:h-[100vh]">
       <Header />
-      <div className="flex flex-col md:items-left items-center w-full md:w-4/5 lg:w-3/5 xl:w-1/2 pt-12 pb-20 md:pb-0 lg:pt-16">
-        <div className="flex flex-col gap-9 pl-12 pt-6 md:items-left items-center ">
-          <h1 className="lg:text-5xl md:text-4xl text-3xl md:w-full lg:w-4/5 md:text-left text-center font-extrabold leading-14">
+      <div className="flex flex-col md:items-left xl:gap-6items-center w-full md:w-4/5 xl:w-1/2 pt-12 pb-20 md:pb-0 2xl:pt-16 ">
+        <div className="flex flex-col 2xl:gap-12 gap-10 md:px-16 pt-6 px-8 md:items-start sm:px-12 items-center">
+          <h1 className="lg:text-5xl md:text-4xl sm:text-3xl text-2xl md:w-full lg:w-4/5 md:text-left text-center font-extrabold md:leading-14">
             Providing the best utilities in DRC
           </h1>
-          <p className="text-xl md:text-left text-center w-4/5">
+          <p className="md:text-left md:text-xl w-4/5 text-center ">
             Go Green with General Energy. We are here to bring smart and sustainable energy solutions with modern technology. 
           </p>
-          <Link href={'#services'} className="w-[230px] text-xl text-center rounded-xl bg-yellow-600 text-blue-800 font-bold px-8 py-4">
-            View Services!
-          </Link>
+          <span>
+            <Link href={'#services'} className="w-[230px] md:text-xl text-center rounded-xl bg-yellow-600 text-blue-800 font-bold px-8 py-4">
+              View Services!
+            </Link>
+          </span>
         </div>
       </div>
     </section>
--- components/Services/index.tsx
+++ components/Services/index.tsx
@@ -4,20 +4,20 @@ import { ProductsAndServices } from "@/constants/data/about"
 
 const Services = () => {
   return (
-    <section id="services" className="flex justify-between items-center gap-9 py-16 px-6">
-      <div className="flex flex-col gap-7">
-        <h3 className="text-4xl font-bold pb-4">Our Services</h3>
-        <span className="text-xl pb-3">We carry out the installation and maintenance of:</span>
-        <ul className="py-8 w-[700px] list-disc list-inside space-y-5 columns-2">
+    <section id="services" className="w-full flex lg:justify-between justify-center items-center xl:gap-9 gap-6 py-16 md:px-20 px-6">
+      <div className="flex flex-col gap-7 items-center lg:items-start">
+        <h3 className="md:text-4xl text-2xl font-bold md:pb-4">Our Services</h3>
+        <span className="md:text-xl pb-3 md:text-left text-center">We carry out the installation and maintenance of:</span>
+        <ul className="md:py-8 py-4 xl:w-[700px] list-disc list-inside space-y-5 sm:columns-2">
          {ProductsAndServices.map((item, idx) => (
            <li className="font-bold" key={idx}>
-              <Link className="hover:underline text-blue-800" key={idx} href={item.url}>{item.name}</Link>
+              <Link className="hover:underline text-blue-800 sm:text-m xl:text-[18px]" key={idx} href={item.url}>{item.name}</Link>
             </li>
           ))}
         </ul>
         <Link className="text-center font-semibold text-blue-900 w-1/2 px-6 py-3 border rounded-xl border-yellow-500" href={'/services'}>Go to services Page &rarr; </Link>
       </div>
-      <div className="self-start">
+      <div className="self-start lg:block hidden">
         <Image
         src={'/images/pexels-kindelmedia-8487406-removebg-preview.png'}
         width={400}
