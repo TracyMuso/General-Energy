dc358bd224b99a6bc8194d612185946b0e370e9f
TracyMuso <musongoletracy@gmail.com>
2024-10-28 12:41:45 +0200

    feat(responsive-page-design): update website with real data

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -15,10 +15,10 @@ export default function ServicesPage() {
         <div className='service-text w-4/5 lg:m-0 md:mt-8 mt-8 md:ml-8 flex flex-col md:justify-between justify-around service-hero-text md:w-1/2 lg:w-2/5 md:pl-12 xl:pl-20 py-5 rounded-xl h-[370px]'>
           <div>
             <h1 className='xl:text-5xl md:text-4xl text-2xl font-bold pb-3 '>General Energy</h1>
-            <p className='lg:text-xl pt-2 pb-8'>Your number one energy and utilities company. We provide modern solutions to modern problems
+            <p className='lg:text-xl pt-2 pb-8'>Your number one energy and utilities company that provides modern solutions to modern problems. Lets transition to green energy together!
             </p>
           </div>
-          <Link href={'#services-container'} className='md:self-baseline mx-auto py-3 px-6 bg-yellow-400 rounded-md font-bold'>View our services below </Link>
+          <Link href={'#services-container'} className='md:self-baseline lg:mx-0mx-auto py-3 px-6 bg-yellow-400 rounded-md font-bold'>View our services below </Link>
         </div>
       </section>
       <section id='services-container' className="w-full flex flex-col items-center lg:items-start py-20 sm:px-12 px-8">
--- components/Cards/ServiceDetsCard.tsx
+++ components/Cards/ServiceDetsCard.tsx
@@ -11,18 +11,18 @@ const ServiceDetsCard = ({ service, maintext, icon, img, point1, point2, point3,
       </div>
           <div className='flex md:items-start items-center sm:w-4/5 md:justify-between justify-center gap-7 py-5 sm:px-9  px-1 border-b-2 border-blue-500'>
             <div className='flex flex-col justify-between gap-3 md:w-3/5 pt-6'>
-              <p className="italic md:text-left text-center">{maintext}</p>
-              <ul className='text-center sm:text-left list-disc space-y-3 list-inside pb-4 md:pt-1 pt-5'>
+              <p className="italic md:text-left md:text-[17px] text-center">{maintext}</p>
+              <ul className='text-left md:text-base text-sm list-disc space-y-3 list-inside pb-4 md:pt-1 pt-5'>
                 <li>{point1}</li>
                 <li>{point2}</li>
                 <li>{point3}</li>
                 <li>{point4}</li>
               </ul>
               <div className='flex flex-col sm:flex-row items-center gap-4'>
-                <Link className='bg-yellow-400 text-white font-bold rounded-2xl py-2 px-5 w-1/2 text-center' href={link}>
+                <Link className='bg-yellow-400 text-white md:text-base text-sm font-bold rounded-2xl py-2 px-5 w-1/2 text-center' href={link}>
                 View Packages
                 </Link>
-                <Link className='border-yellow-500 border font-bold rounded-2xl py-2 px-5 w-1/2 text-center' href={'#'}>
+                <Link className='border-yellow-500 border md:text-base text-sm font-bold rounded-2xl py-2 px-5 w-1/2 text-center' href={'#'}>
                 View Projects
                 </Link>
               </div>
