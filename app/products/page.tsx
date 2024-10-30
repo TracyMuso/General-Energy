c6fd57930a4c4c19e70fc268d22a16125a2745df
TracyMuso <musongoletracy@gmail.com>
2024-10-30 16:30:13 +0200

    feat(products-page): make products card mobile responsive

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,10 +4,11 @@ import Header from "@/components/Layout/Header"
 import MainProductCard from "@/components/Cards/MainProductCard"
 
 export default function ProductsPage() {
+
   return (
     <section className='w-full'>
       <Header />
-      <div className='py-8 w-full flex flex-col gap-10 px-16'>
+      <div className='py-8 w-full flex flex-col gap-10 lg:px-16 md:px-12 px-8'>
         <MainProductCard />
         <MainProductCard />
         <MainProductCard />
--- components/Cards/MainProductCard.tsx
+++ components/Cards/MainProductCard.tsx
@@ -1,36 +1,44 @@
+import React, {useEffect} from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 const MainProductCard = () => {
+  useEffect(() => {
+    AOS.init();
+  }, [])
+
   return (
-    <div className="flex items-start justify-between gap-12 px-5 border-b-2 border-blue-300">
-      <div className="w-[400px] h-[450px] p-4">
-        <img src={'/images/brown-building.jpg'} alt="img" />
+    <div className="flex md:flex-row flex-col md:items-start justify-between lg:gap-12 md:gap-4 lg:px-5 px-2 xl:pb-6 border-b-2 border-blue-300">
+      <div className="lg:w-[500px] lg:h-[450px] md:w-[400px] h-[350px] md:h-[400px] p-4">
+        <img className="w-full h-full rounded-xl" src={'/images/brown-building.jpg'} alt="img" />
       </div>
-      <div className="flex flex-col gap-3 p-4">
-        <h3 className="font-bold text-xl">Hybrid Solar Inverter - 3kva + battery</h3>
+      <div className="flex flex-col xl:gap-3 gap-1 p-4 2xl:w-auto md:w-3/5">
+        <h3 className="md:font-bold font-semi-bold text-xl xl:text-2xl">Hybrid Solar Inverter - 3kva + battery</h3>
         <span className="font-semibold text-base">$10</span>
-        <span className="text-red-500">Out of stock</span>
-        <span className="text-blue-600 italic">Free delivery within Kinshasa
+        <span className="text-red-500 xl:text-[17px] text-sm">Out of stock</span>
+        <span className="text-blue-600 italic xl:text-[17px] text-sm">Free delivery within Kinshasa
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 inline ml-4">
           <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
         </svg>
         <div className="py-2">
           <span className="underline">Variants</span>
           <ul className="flex items-center gap-5 py-1">
-            <li className="rounded-lg border border-blue-300 py-1 px-2 text-sm hover:bg-blue-200 cursor-pointer">5kva</li>
-            <li className="rounded-lg border border-blue-300 py-1 px-2 text-sm hover:bg-blue-200 cursor-pointer">8kva</li>
-            <li className="rounded-lg border border-blue-300 py-1 px-2 text-sm hover:bg-blue-200 cursor-pointer">10kva</li>
-            <li className="rounded-lg border border-blue-300 py-1 px-2 text-sm hover:bg-blue-200 cursor-pointer">12kva</li>
+            <li className="rounded-lg border border-blue-300 py-1 px-2 xl:text-sm text-[11px] hover:bg-blue-200 cursor-pointer">5kva</li>
+            <li className="rounded-lg border border-blue-300 py-1 px-2 xl:text-sm text-[11px] hover:bg-blue-200 cursor-pointer">8kva</li>
+            <li className="rounded-lg border border-blue-300 py-1 px-2 xl:text-sm text-[11px] hover:bg-blue-200 cursor-pointer">10kva</li>
+            <li className="rounded-lg border border-blue-300 py-1 px-2 xl:text-sm text-[11px] hover:bg-blue-200 cursor-pointer">12kva</li>
           </ul>
         </div>
         </span>
         <hr />
-        <ul className="list-disc list-inside space-y-3">
-          <li>
+        <ul className="list-disc pl-3 space-y-3">
+          <li className="xl:text-base sm:text-m text-sm">
             When mains power is on this inverter will automatically kick in and start charging your batteries.
           </li>
-          <li>
-            During loadshedding this inverter will automatically switch over to run off battery power and your appliances will continue to have power.
+          <li className="xl:text-base sm:text-m text-sm">
+            During loadshedding this inverter will automatically runs off battery power and your appliances will continue to have power.
           </li>
-          <li>
+          <li className="xl:text-base sm:text-m text-sm">
             Includes battery cables to connect battery and inverter.
           </li>
         </ul>
